df9b14e952f50120819242e0748b76b6ab106c0f
matthieu rochard <matthieurochard@yahoo.fr>
2023-01-17 15:57:58 +0100

    10% of the original db

--- src/GetAnswers.py
+++ src/GetAnswers.py
@@ -5,8 +5,8 @@ import cloudscraper
 from bs4 import BeautifulSoup
 from lxml import html
 
-LAWYER_PATH = './data/lawyerid_to_lawyerurl.json'
-QUESTIONS_PATH = './data/question_links_hole.json'
+
+QUESTIONS_PATH = './data/question_links_bankruptcy.json'
 #QUESTIONS_PATH = '../data/q.json'
 
 def get_answers(doc: str) -> dict:
@@ -21,19 +21,21 @@ def get_answers(doc: str) -> dict:
 	soup = BeautifulSoup(doc, 'html.parser')
 	answers = soup.find_all('div', {'class':'qa-answer'})
 	# get div with id = qa-subject-display
-	post = soup.find('div',{ 'id':'qa-subject-display'})
+	try:
+		post = soup.find('div',{ 'id':'qa-subject-display'}).text.strip()
+	except:
+		raise Exception("Error: no post found in the page: "+doc)
 	#print(post)
 	# get the question
-	title = post.find('h1').text
-	text_question = post.find('div', {'id':'question-body'}).text
+	#title = post.find('h1').text
+	#text_question = post.find('div', {'id':'question-body'}).text
 	#resp = html.fromstring(doc)
 	#answers = resp.xpath('div[@class="card qa-lawyer-card qa-answer qa-bordertop"]//div[@class="js-answer"]')
 	#xpath('//*[@class="answer-body is-truncated"]/p')
 	#print("--"+len(answers))
 	for i in range(0, len(answers)):
 		a = {
-			'title': title,
-			'post': text_question,
+			'post': post,
 		}
 		#print(answers[i])
 		try:
@@ -44,18 +46,21 @@ def get_answers(doc: str) -> dict:
 			#print(resp[i].xpath('//div[@class="row answer-professional"]//a[@class="small gtm-subcontext"]/@href'))
 		if not author_id in new_answers:
 			new_answers[author_id] = []
-		#a['response'] = resp[i].xpath('//div[@class="answer-body is-truncated"]/p').text
-		a['response'] = answers[i].find('div', class_='answer-body').find('p').text
+		#a['response'] = resp[i].xpath('//div[@class="answer-body is-truncated"]/p').text answer-upvote-section
+		a['response'] = answers[i].find('div', class_='answer-body').find('p').text.strip()
+		votes = answers[i].find('span', class_='answer-upvote-section').find_all('span')
+		#print(votes)
+		a['upvotes'] = int(votes[4].text.replace(' lawyer agrees','').replace(' lawyers agree',''))
+		a['helpful'] = int(votes[0].text)
+		if len(votes) > 6:
+			a['most_helpful'] = "Voted as Most Helpful" in votes[6].text
+		else:
+			a['most_helpful'] = False
 		#new_answers[author_id].append(resp[i].xpath('//div[@class="answer-body is-truncated"]/p').text)
 		new_answers[author_id].append(a)
 		#a['author'] = answer.find('div', class_='answer-professional').find('a').get('href')
 
 	return new_answers
-
-def get_lawyer_data():
-	lawyer_path = LAWYER_PATH
-	lawyer = json.load(open(lawyer_path, 'r'))
-	return lawyer
 
 def get_questions_data():
 	questions_path = QUESTIONS_PATH
@@ -65,58 +70,48 @@ def get_questions_data():
 class Preprocessor:
 
 	def __init__(self):
-		self.lawyer = get_lawyer_data()
+		#self.lawyer = get_lawyer_data()
 		self.questions = get_questions_data()
 		#self.text_analyzer = TextAnalyzer()
 		#self.scraper = cloudscraper.create_scraper(delay=10, browser='chrome',disableCloudflareV1=False)
 		self.scraper = cloudscraper.create_scraper()
 
-	def create_lawyer_data_from_url(self):
-		data = []
-		# download the data from the url
-		for lawyer_id, lawyer_url in self.lawyer.items():
-			try:
-				r = self.scraper.get(lawyer_url)
-				#r = self.text_analyzer.normalize(r.text,remove_html_tag=True)
-				r = {"content": r}
-				data.append(r)
-			except Exception as e:
-				print("!!"+e)
-		# save the data
-		with open('./data/lawyer_data.json', 'w') as f:
-			json.dump(data, f)
-
 	def create_questions_data_from_url(self):
 		data = {}
 		# download the data from the url
 		c = 0
+		deleted_questions = 0
 		for page_id, question_urls in self.questions.items():
 			print(page_id)
 			i=0
-			c +=1
 			for question_url in question_urls:
 				print(i)
 				i+=1
+				r = self.scraper.get(question_url)
+				#r = self.text_analyzer.normalize(r.text,remove_html_tag=True)
 				try:
-					r = self.scraper.get("https://www.avvo.com"+question_url)
-					#r = self.text_analyzer.normalize(r.text,remove_html_tag=True)
 					aws = get_answers(r.text)
-					#c += len(aws)
-					# merge the data
 					for k,v in aws.items():
 						if k in data:
 							data[k].extend(v)
 						else:
 							data[k] = v
-
-				except Exception as e:
-					print("!"+e)
-			#with open('./data/pages/question_data_'+ str(c) +'.json', 'w') as f:
-			#	json.dump(data, f)
-			
-		with open('./data/answers_data.json', 'w') as f:
+				
+				except:
+					print("error in page: " + page_id + " question: " + question_url +" the question likely has been deleted")
+					deleted_questions +=1
+					continue
+					#raise Exception("error in page: " + page_id + " question: " + question_url)
+				#c += len(aws)
+				# merge the data
+				c +=1
+				if(c%100==0):
+					with open('./data/pages/lawyer_answers_data_'+ str(c) +'.json', 'w') as f:
+						json.dump(data, f)
+		
+		with open('./data/lawyer_answers_data.json', 'w') as f:
 			json.dump(data, f)
-
+		print(str(deleted_questions)+" questions have been deleted out off "+str(c+deleted_questions)+" questions")
 
 	def create_data(self):
 		#self.create_lawyer_data_from_url()
---
+++ src/analyseOurData_script.py
@@ -0,0 +1,32 @@
+import json
+
+#
+# 9018 questions have been deleted out off 9942 questions, so only 924 questions are left aka 9.3% of the questions
+#
+if __name__ == "__main__":
+	out_data = json.load(open('./data/lawyer_answers_data.json', 'r'))
+	
+	our_lawyers = list(out_data.keys())
+	i=0
+	for k in our_lawyers:
+		i += len(out_data[k])
+	print("We have gathered "+ str(i) +" answers")
+	their_data=json.load(open('./data/lawyerid_to_lawyerurl.json', 'r'))
+	their_data = [v for k,v in their_data.items()]
+	
+	their_lawyers = []
+	for lawyer in their_data:
+		their_lawyers.append(lawyer.replace("https://www.avvo.com/attorneys/","").replace(".html",""))
+	their_lawyers = set(their_lawyers)
+	our_lawyers = set(our_lawyers)
+	print("--------------------")
+	print("We have gathered "+str(len(our_lawyers))+" lawyers")
+	print("They gathered "+str(len(their_lawyers))+" lawyers")
+	print("Or "+str(len(our_lawyers)/len(their_lawyers)*100)+"% of their lawyers")
+	missing = their_lawyers - our_lawyers
+	print("--------------------")
+	print("We are missing "+str(len(missing))+" lawyers in our data out off their "+str(len(their_lawyers))+" lawyers ("+str(len(missing)/len(their_lawyers)*100)+"%)")
+	print("We have "+str(len(our_lawyers - their_lawyers))+" lawyers that they don't have")
+	
+
+
